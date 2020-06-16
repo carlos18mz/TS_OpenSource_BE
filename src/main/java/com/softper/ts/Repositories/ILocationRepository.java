88e9f6154eb250020dee3ddfa85b64244fecefbb
Cmz <hollowmask18300@gmail.com>
2020-06-15 22:51:07 -0500

    Implementado estimar ruta de pedido

--- src/main/java/com/softper/ts/Repositories/ILocationRepository.java
+++ src/main/java/com/softper/ts/Repositories/ILocationRepository.java
@@ -1,7 +1,18 @@
 package com.softper.ts.Repositories;
 
+import com.softper.ts.Model.Favourite;
 import com.softper.ts.Model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ILocationRepository extends JpaRepository<Location, Integer> {
+
+    @Query("select l from Location l where l.cargo.id = (:uid)")
+    Location findLocationByCargoId(@Param("uid") int cargoId);
+
+    @Query("select l from Location l where l.route.id = (:uid)")
+    Location findLocationByRouteId(@Param("uid") int routeId);
 }

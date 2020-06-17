87746c226aba8eec915092c055b853892c71b59f
Cmz <hollowmask18300@gmail.com>
2020-06-17 02:18:17 -0500

    Implementar realizar registro

--- src/main/java/com/softper/ts/ServicesImp/AuthService.java
+++ src/main/java/com/softper/ts/ServicesImp/AuthService.java
@@ -42,7 +42,70 @@ public class AuthService implements IAuthService {
 
     @Override
     public AuthResponse RegisterComplete(SignUp signUp) {
-        return null;
+        try
+        {
+            Person newPerson = new Person();
+            newPerson.setFirstName(signUp.getFirstName());
+            newPerson.setLastName(signUp.getLastName());
+            newPerson.setPhone(signUp.getPhone());
+            newPerson.setPersonType(signUp.getDiscriminator());
+            newPerson = personRepository.save(newPerson);
+
+            Configuration newConfiguration = new Configuration();
+            newConfiguration.setLanguage("Spanish");
+            newConfiguration.setPaymentCurrency("Soles");
+
+            newConfiguration = configurationRepository.save(newConfiguration);
+
+            Balance newBalance = new Balance();
+            newBalance.setSpentMoney(0);
+            newBalance.setAddedMoney(0);
+
+            newBalance = balanceRepository.save(newBalance);
+
+            User user = new User();
+            user.setEmail(signUp.getEmail());
+            user.setPassword(signUp.getPassword());
+            user.setCreatedAt(Calendar.getInstance().getTime());
+            user.setPerson(newPerson);
+            user.setConfiguration(newConfiguration);
+            user.setBalance(newBalance);
+
+            user = userRepository.save(user);
+
+            if(signUp.getDiscriminator() == 1) {
+                Customer newCustomer = new Customer();
+                newCustomer.setCredits(0.0);
+                newCustomer.setPerson(newPerson);
+                newPerson.setCustomer(newCustomer);
+
+                customerRepository.save(newCustomer);
+            }
+            else
+            {
+                Driver newDriver = new Driver();
+                newDriver.setLicense("000-123");
+                newDriver.setPerson(newPerson);
+                newPerson.setDriver(newDriver);
+
+                Qualification newQualication = new Qualification();
+                newQualication.setDriver(newDriver);
+
+                ServiceRequest newServiceRequest = new ServiceRequest();
+                newServiceRequest.setDriver(newDriver);
+
+                qualificationRepository.save(newQualication);
+                serviceRequestRepository.save(newServiceRequest);
+                driverRepository.save(newDriver);
+            }
+            return new AuthResponse(new AuthenticatedOutput(user.getId(),user.getEmail(),user.getPassword(),newPerson.getFirstName(),newPerson.getLastName(),newPerson.getPersonType()));
+        }
+        catch (Exception e)
+        {
+            return new AuthResponse("An error ocurred while register the user: "+e.getMessage());
+        }
+
+
     }
 
     @Override
